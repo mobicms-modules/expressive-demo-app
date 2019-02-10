1a60e47d25113a5c3017f0dc6fc184d2d0b6d36b
Oleg Kasyanov <dev@mobicms.net>
2019-02-10 16:33:14 +0400

    Added session health check

--- src/Handler/PingHandler.php
+++ src/Handler/PingHandler.php
@@ -10,11 +10,22 @@ use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 use function time;
+use Zend\Expressive\Session\SessionInterface;
 
 class PingHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        return new JsonResponse(['ack' => time()]);
+        /** @var SessionInterface $session */
+        $session = $request->getAttribute('session');
+
+        if($session->has('test')){
+            $test = $session->get('test');
+        } else {
+            $test = '---';
+            $session->set('test', 'session-test-value');
+        }
+
+        return new JsonResponse(['ack' => time(), 'session' => $test]);
     }
 }
